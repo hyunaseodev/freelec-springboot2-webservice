fe0c4ac89d86f571d0ffad330e4154452a819ef2
HYUNA <hyunaseodev@gmail.com>
2022-03-25 17:48:48 +0900

    로그인 연동 실패

--- src/main/java/com/jojoldu/book/springboot/domain/BaseTimeEntity.java
+++ src/main/java/com/jojoldu/book/springboot/domain/BaseTimeEntity.java
@@ -4,10 +4,8 @@ import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
